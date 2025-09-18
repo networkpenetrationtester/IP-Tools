923f90de27d6f467734213000bb5ac13bc8091d7
Neo <77405346+networkpenetrationtester@users.noreply.github.com>
2025-09-18 14:06:04 -0500

    Update subnetInformation.js

--- subnetInformation.js
+++ subnetInformation.js
@@ -28,15 +28,27 @@ class InvalidIPAddressError extends Error {
     }
 }
 
-class InvalidOctetBorderPrefixLengthError extends Error {
+class InvalidOctetBorderError extends Error {
     constructor(octet_border) {
-        super(`Invalid Octet Border/Prefix Length: ${octet_border}`);
+        super(`Invalid Octet Border: ${octet_border}`);
+    }
+}
+
+class InvalidPrefixLengthError extends Error {
+    constructor(prefix_length) {
+        super(`Prefix Length: ${prefix_length}`);
     }
 }
 
 class InvalidSubnetMaskError extends Error {
     constructor(subnet_mask) {
         super(`Invalid Subnet Mask: ${subnet_mask}`);
+    }
+}
+
+class InvalidMacAddressError extends Error {
+    constructor(mac_address) {
+        super(`Invalid MAC Address: ${mac_address}`);
     }
 }
 
@@ -48,100 +60,105 @@ class MalformedInputError extends Error {
 
 /**
  * 
- * @param {*} input IPv4 range formatted such as 10.0.0.0/8
+ * @param {*} ip_address_range IPv4 range formatted such as 10.0.0.0/8
  * @returns if this is a valid range.
  */
 
-function validateIPv4Range(input) {
-    if (!input.match('/')) {
-        throw new MalformedInputError(input);
+function validateIPv4Range(ip_address_range) {
+    if (!ip_address_range.match('/')) {
+        throw new MalformedInputError(ip_address_range);
     }
-
-    let parts = input.split('/');
+    let parts = ip_address_range.split('/');
     let network_id = parts[0];
     let octet_border = parseInt(parts[1]);
-
+    let subnet_mask = prefixLengthToSubnetMask(octet_border);
     if (octet_border > 32 || octet_border < 0) {
-        throw new InvalidOctetBorderPrefixLengthError(octet_border);
+        throw new InvalidOctetBorderError(octet_border);
     }
-
     let network_octets_binary = '';
-
-
     let network_octets = network_id.split('.').map((octet) => {
         octet = parseInt(octet);
-        if (isNaN(octet) || octet > 255 || octet < 0) throw new Error("Invalid IP Address.");
+        if (isNaN(octet) || octet > 255 || octet < 0) throw new InvalidIPAddressError(ip_address_range);
         let padding = new Array(8).fill(0);
 
         let octet_binary = octet.toString(2);
         return oct;
     });
-
     console.log([parts, network_id, octet_border, network_octets].join('\n'));
-
     // '/8' signifies that the left-most 8 bits (1st network octet) should be reserved.
     // This means that the 1st network octet must have a value <= 2^octet_bound - 1
     // in this case, 10 should pass.
-
     // suppose you had a 10.0.0.0/2 as input. 2^2-1 = 3. This should fail.
 
 }
 
-function validateSubnetMask(subnet_mask) {
-    validateIPv4Address(subnet_mask);
-    let subnet_mask_binary = dottedDecimalToBinary(subnet_mask);
-    let prefix_finished = false;
-    for (let char of subnet_mask_binary) {
-        if (char === '0' && !prefix_finished) {
-            prefix_finished = true;
-            continue;
-        }
-        if (prefix_finished && char === '1') throw new Error("What the fuck?");
-    }
-    return true;
-}
+/**
+ * Input MAC Addresses as XX::XX::XX::XX::XX::XX
+ * @param {*} mac_address 
+ */
 
-function validateIPv4Address(ip_address, binary) { // maybe also check for valid subnet masks.
-    let ip_address_octets = ip_address.split('.');
-    if (ip_address_octets.length !== 4) throw new InvalidIPAddressError(ip_address);
-    ip_address_octets.forEach((octet) => {
-        let octet_parsed = parseInt(octet, binary ? 2 : 10);
-        if (isNaN(octet_parsed) || octet_parsed > 255 || octet_parsed < 0 || binary && octet.length > 8) throw new InvalidIPAddressError(ip_address); //throw new Error("Invalid IP Address.");
+function validateMACAddress(mac_address) {
+    let mac_address_octets = mac_address.split('::');
+    if (mac_address_octets.length !== 6) throw new InvalidMacAddressError(mac_address);
+    mac_address_octets.forEach((octet) => {
+        let octet_parsed = parseInt(octet, 16);
+        if (isNaN(octet_parsed) || octet_parsed > 255 || octet_parsed < 0) throw new InvalidMacAddressError(mac_address);
     });
     return true;
 }
 
-function dottedDecimalToBinary(ip_dottedDecimal) {
-    validateIPv4Address(ip_dottedDecimal);
-    let ip_binary = ip_dottedDecimal.split('.').map((decimal) => parseInt(decimal, 10).toString(2).padStart(8, '0'));
+function validateDottedDecimal(dotted_decimal, binary = false, subnet = false) { // maybe also check for valid subnet masks.
+    let octets = dotted_decimal.split('.');
+    let error_type = subnet ? InvalidSubnetMaskError : InvalidIPAddressError;
+    if (octets.length !== 4) throw new error_type(dotted_decimal);
+    
+    octets.forEach((octet) => {
+        let octet_parsed = parseInt(octet, binary ? 2 : 10);
+        let octet_valid = (binary == (octet.length == 8)) || isNaN(octet_parsed) || octet_parsed > 255 || octet_parsed < 0;
+        if(!octet_valid) throw new error_type(dotted_decimal);
+    });
+    return true;
+}
+
+function EUI_64(ipv6_network_prefix, mac_address) {
+
+}
+
+function IPv6DoubleColon(){
+
+}
+
+function dottedDecimalToBinary(ip_dotted_decimal) {
+    validateDottedDecimal(ip_dotted_decimal);
+    let ip_binary = ip_dotted_decimal.split('.').map((decimal) => parseInt(decimal, 10).toString(2).padStart(8, '0'));
     return ip_binary.join('.');
 }
 
 function binaryToDottedDecimal(ip_binary) {
-    validateIPv4Address(ip_binary, true);
-    let ip_dottedDecimal = ip_binary.split('.').map((byte) => parseInt(byte, 2).toString(10)).join('.');
-    return ip_dottedDecimal;
+    validateDottedDecimal(ip_binary, true);
+    let ip_dotted_decimal = ip_binary.split('.').map((byte) => parseInt(byte, 2).toString(10)).join('.');
+    return ip_dotted_decimal;
 }
 
 function getNetworkAddress(ip_address, subnet_mask) {
     let ip_address_binary = dottedDecimalToBinary(ip_address).split('.');
     let subnet_mask_binary = dottedDecimalToBinary(subnet_mask).split('.');
     let network_address = [];
-
     for (let i = 0; i < 4; i++) {
         network_address.push(parseInt(ip_address_binary[i], 2) & parseInt(subnet_mask_binary[i], 2));
     }
-
     return network_address.join('.');
 }
 
 function subnetMaskToPrefixLength(subnet_mask) {
-    validateSubnetMask(subnet_mask);
+    validateDottedDecimal(subnet_mask, false, true);
     let subnet_mask_binary = dottedDecimalToBinary(subnet_mask);
-    let prefix_length = 0;
-    for (let i = 0; i < 32; i++) subnet_mask_binary[i] === '1' && prefix_length++;
+    prefix_length ??= 0;
+    let prefix_length = subnet_mask_binary.match(/1/g)?.length;
     return prefix_length;
 }
+
+// download the map of the internet as desktop wallpaper.
 
 function prefixLengthToSubnetMask(prefix_length) {
     let subnet_mask_binary = '';
@@ -155,8 +172,8 @@ function prefixLengthToSubnetMask(prefix_length) {
 }
 
 function subnetInformation(octet_border, prefix_length = 32) {
-    if (prefix_length > 32 || prefix_length < 0) throw new InvalidOctetBorderPrefixLengthError(prefix_length);
-    if (!(octet_border % 8 == 0) || octet_border < 8 || octet_border > 32) throw new InvalidOctetBorderPrefixLengthError(octet_border);
+    if (prefix_length > 32 || prefix_length < 0) throw new InvalidPrefixLengthError(prefix_length);
+    if (!(octet_border % 8 == 0) || octet_border < 8 || octet_border > 32) throw new InvalidOctetBorderError(octet_border);
     let obj = {};
     obj.host_bits_allocated = 32 - octet_border;
     obj.host_bits_borrowed = prefix_length - octet_border;
@@ -164,20 +181,17 @@ function subnetInformation(octet_border, prefix_length = 32) {
     obj.subnet_count = 1 << obj.host_bits_borrowed;
     obj.subnet_allocated_addresses = 1 << obj.host_bits_remaining
     obj.subnet_allocated_hosts = obj.subnet_allocated_addresses - 2;
-
     let subnet_mask_binary = '';
     for (let i = 0; i < 32; i++) {
         let on = i < octet_border + obj.host_bits_borrowed;
         subnet_mask_binary += on ? '1' : '0';
         if (i < 31 && (i + 1) % 8 == 0) subnet_mask_binary += '.';
     }
-
     obj.subnet_mask_binary = subnet_mask_binary;
     obj.subnet_mask_decimal = subnet_mask_binary.split(/\./).map((octet) => {
         return parseInt(octet, 2);
     }).join('.');
-
     return obj;
 }
 
-export { validateIPv4Address, subnetInformation, prefixLengthToSubnetMask, subnetMaskToPrefixLength };
+export { validateDottedDecimal as validateIPv4Address, subnetInformation, prefixLengthToSubnetMask, subnetMaskToPrefixLength };
